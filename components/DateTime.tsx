a85b44aaea89fdd50bfa0d3c26954c93b4b51af8
C:\Marcel.exe <fujitsustar@hotmail.de>
2024-07-26 18:12:28 +0200

    feat clock for time via react-clock

--- components/DateTime.tsx
+++ components/DateTime.tsx
@@ -2,7 +2,9 @@
 
 import moment from "moment";
 import { useEffect, useState } from "react";
+import Clock from "react-clock";
 import { clearInterval, setInterval } from "timers";
+import "react-clock/dist/Clock.css";
 
 export const DateTime = () => {
   const [date, setDate] = useState<Date>(new Date());
@@ -18,19 +20,22 @@ export const DateTime = () => {
   }, []);
 
   return (
-    <>
-      <time dateTime={date.toISOString()}>{date.toLocaleString()}</time>
-      <div className="text-xl">
-        {moment().format("MMMM Do YYYY, h:mm:ss a")}
-        <p>
-          Full :{" "}
-          {new Intl.DateTimeFormat("en-GB", {
-            dateStyle: "full",
-            timeStyle: "long",
-          }).format(date)}
-        </p>
-      </div>
-    </>
+    // <>
+    //   <time dateTime={date.toISOString()}>{date.toLocaleString()}</time>
+    //   <div className="text-xl">
+    //     {moment().format("MMMM Do YYYY, h:mm:ss a")}
+    //     <p>
+    //       Full :{" "}
+    //       {new Intl.DateTimeFormat("en-GB", {
+    //         dateStyle: "full",
+    //         timeStyle: "long",
+    //       }).format(date)}
+    //     </p>
+    //   </div>
+    // </>
+    <div>
+      <Clock renderNumbers={true} value={date} />
+    </div>
   );
 };
 
