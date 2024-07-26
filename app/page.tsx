587aaa294bca4cf0aa88f743e6242a9c3612f42a
C:\Marcel.exe <fujitsustar@hotmail.de>
2024-07-26 15:24:46 +0200

    feat simple grid layout with placeholders

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import DateTime from "@/components/DateTime";
+
 export default function Home() {
-  return;
+  return (
+    <div className="grid grid-cols-3 grid-rows-3 gap-4 h-screen">
+      <div>1</div>
+      <div>2</div>
+      <div>
+        <DateTime />
+      </div>
+      <div className="col-span-3">4</div>
+      <div className="col-span-3 row-start-4">5</div>
+    </div>
+  );
 }
