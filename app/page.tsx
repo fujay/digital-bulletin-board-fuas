745e77a2d7d21aaeeb32f8fd5d9e0ffa07cdef24
C:\Marcel.exe <fujitsustar@hotmail.de>
2024-07-29 14:39:08 +0200

    Add hyperlink as optimized Link & Image for dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import DateTime from "@/components/DateTime";
+import Image from "next/image";
+import Link from "next/link";
+
+import logo from "@/assets/logo-fuas.png";
 
 export default function Home() {
   return (
     <div className="grid grid-cols-3 grid-rows-3 gap-4 h-screen">
-      <div>1</div>
+      <div>
+        <Link href={"/dasboard"}>
+          <Image src={logo} alt="FUAS logo" priority />
+        </Link>
+      </div>
       <div>2</div>
       <div>
         <DateTime />
