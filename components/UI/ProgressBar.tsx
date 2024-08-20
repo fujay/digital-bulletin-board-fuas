8e72ac97c97f45c21831a63c313bfb036ea455ca
C:\Marcel.exe <fujitsustar@hotmail.de>
2024-08-20 05:22:18 +0200

    feat: Add ProgressBar component for displaying progress

---
+++ components/UI/ProgressBar.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function ProgressBar() {
+  const ContextObj = {
+    duration: 10000,
+  };
+
+  const [remainingTime, setRemainingTime] = useState(ContextObj.duration);
+
+  useEffect(() => {
+    let timer = setInterval(function () {
+      setRemainingTime((prevTime) => {
+        if (prevTime <= 0) {
+          clearInterval(timer);
+          return prevTime;
+        }
+        return prevTime - 50;
+      });
+    }, 50);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  console.log(remainingTime);
+
+  return (
+    <progress
+      className="w-full [&::-webkit-progress-bar]:rounded-lg [&::-webkit-progress-value]:rounded-lg   [&::-webkit-progress-bar]:bg-fuas-secondary [&::-webkit-progress-value]:bg-fuas-primary [&::-moz-progress-bar]:bg-fuas-primary "
+      max={ContextObj.duration}
+      value={remainingTime}
+    />
+  );
+}
