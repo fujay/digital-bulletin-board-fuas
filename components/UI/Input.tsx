ad54c66158ac8a45678a40aaee9172ff1327266f
C:\Marcel.exe <fujitsustar@hotmail.de>
2024-09-04 16:11:00 +0200

    feat: Improve Input component default styling and optional icon and tooltip (WiP) support

--- components/UI/Input.tsx
+++ components/UI/Input.tsx
@@ -1,16 +1,34 @@
 import { ComponentPropsWithoutRef } from "react";
-
+import { IconType } from "react-icons";
+/* City or Coordinates as &apos;lat,lon&apos; like 50.11552, 8.68417 */
 type InputProps = {
   label: string;
   id: string;
+  icon?: IconType;
+  tooltip?: string;
 } & ComponentPropsWithoutRef<"input">;
 
-function Input({ label, id, ...props }: InputProps) {
+function Input({ label, id, icon: Icon, tooltip, ...props }: InputProps) {
   return (
-    <p>
+    <div>
       <label htmlFor={id}>{label}</label>
-      <input id={id} name={id} {...props} />
-    </p>
+      {tooltip && <label className="text-xs"> (?)</label>}
+      <div className="relative mt-2">
+        {Icon && (
+          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+            <span className="text-fuas-secondary ">
+              <Icon />
+            </span>
+          </div>
+        )}
+        <input
+          className="block border text-fuas-primary border-fuas-secondary focus:border-fuas-primary rounded focus:outline-none w-full px-4 py-2 pl-10"
+          id={id}
+          name={id}
+          {...props}
+        />
+      </div>
+    </div>
   );
 }
 
