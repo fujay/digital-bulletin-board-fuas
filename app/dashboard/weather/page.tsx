9c755e63dc803f1d1d07a78aaefcb9d7ac3f1c28
C:\Marcel.exe <fujitsustar@hotmail.de>
2024-09-07 20:32:27 +0200

    Added page for weather header and config

---
+++ app/dashboard/weather/page.tsx
@@ -0,0 +1,11 @@
+import Heading from "@/components/UI/Heading";
+import WeatherConfig from "@/components/WeatherConfig";
+
+export default function Weather() {
+  return (
+    <>
+      <Heading />
+      <WeatherConfig />
+    </>
+  );
+}
