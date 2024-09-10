311da5d3012b44e5087dbb8eb732daacf711f884
C:\Marcel.exe <fujitsustar@hotmail.de>
2024-09-10 18:51:18 +0200

    feat: Add functions for reading and saving config data

---
+++ lib/useConfig.ts
@@ -0,0 +1,22 @@
+import fs from "node:fs/promises";
+
+export async function readConfig() {
+  const configFileContent = await fs.readFile("configData.json", "utf-8"); //./
+  const config = JSON.parse(configFileContent);
+  return config;
+}
+
+export async function readKeyConfig(key: string) {
+  const configFileContent = await fs.readFile("configData.json", "utf-8"); //./
+  const config = JSON.parse(configFileContent);
+  const keyConfig = config[key];
+  return keyConfig;
+}
+
+export async function saveConfig(newConfig: {}, key: string) {
+  const configFileContent = await fs.readFile("configData.json", "utf-8");
+  const config = JSON.parse(configFileContent);
+  config[key] = newConfig;
+
+  await fs.writeFile("configData.json", JSON.stringify(config));
+}
