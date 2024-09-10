fcaad388934aef82b39b42297b304d2d188d52f6
C:\Marcel.exe <fujitsustar@hotmail.de>
2024-09-10 19:20:45 +0200

    Refactor Weather page to read and display configuration data

--- app/dashboard/weather/page.tsx
+++ app/dashboard/weather/page.tsx
@@ -1,11 +1,17 @@
 import Heading from "@/components/UI/Heading";
 import WeatherConfig from "@/components/WeatherConfig";
 
-export default function Weather() {
+import { readKeyConfig } from "@/lib/useConfig";
+
+export default async function Weather() {
+  const { location, qrcode, graphic } = (await readKeyConfig(
+    Weather.name.toLowerCase()
+  )) || { location: "", qrcode: "", graphic: "Classic" };
+
   return (
     <>
       <Heading />
-      <WeatherConfig />
+      <WeatherConfig location={location} qrcode={qrcode} graphic={graphic} />
     </>
   );
 }
