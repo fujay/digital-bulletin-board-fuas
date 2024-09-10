9968521c4dfe0e6ef96d2f6d33f5732bd415c2e6
C:\Marcel.exe <fujitsustar@hotmail.de>
2024-09-10 06:00:40 +0200

    style: removed commented out code

--- components/nav-link.tsx
+++ components/nav-link.tsx
@@ -19,11 +19,6 @@ export default function NavLink({ href, children }: NavLinkProps) {
           ? "text-fuas-secondary bg-fuas-primary"
           : "bg-neutral-200/70 text-fuas-primary hover:text-fuas-secondary hover:bg-fuas-primary"
       } font-bold p-4 rounded transition-all hover:shadow-xl focus:outline-none focus:ring focus:border-fuas-primary`}
-      // className={`${
-      //   path === href
-      //     ? "text-blue-800 bg-fuas-secondary rounded-xl"
-      //     : "text-fuas-primary bg-gray-400 hover:bg-fuas-secondary hover:rounded-xl"
-      // } font-bold p-4 rounded-3xl transition-all hover:shadow-xl focus:outline-none focus:ring focus:border-fuas-primary`}
     >
       {children}
     </Link>
