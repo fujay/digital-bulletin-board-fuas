804ab11e3f1150bb26da4fa3c3d908d0e6adeab5
C:\Marcel.exe <fujitsustar@hotmail.de>
2024-09-19 06:04:23 +0200

    refactor: Update tailwind.config.ts for animations classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,53 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			'fuas-primary': '#2D89CC',
-  			'fuas-secondary': '#B6D2E4'
-  		},
-  		backgroundImage: {
-  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      colors: {
+        "fuas-primary": "#2D89CC",
+        "fuas-secondary": "#B6D2E4",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        appear: {
+          from: {
+            opacity: "0",
+            transform: "translateY(4rem)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateY(0%)",
+          },
+        },
+        slide: {
+          from: {
+            transform: "translateY(-100%)",
+          },
+          to: {
+            transform: "translateY(0%)",
+          },
+        },
+      },
+      animation: {
+        slide: "slide 750ms ease-in-out",
+        appear: "appear 750ms ease-in-out",
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms"), require("tailwindcss-animate")],
 };
